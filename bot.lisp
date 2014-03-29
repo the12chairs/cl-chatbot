dee38e49981dc0d540478b027c469830e88ab260
the12chairs <mbondarenko92@gmail.com>
2014-03-29 18:18:32 +0700

    Good bot

--- bot.lisp
+++ bot.lisp
@@ -26,6 +26,24 @@
     prepared-phrase))
 
 
+(defun get-prior (key)
+  (let ((query-for-word (concatenate 'string
+				     "SELECT priority FROM keywords WHERE word = \""
+				     (princ-to-string key)
+				     "\"")))
+   (car (caaar (cl-mysql:query query-for-word)))))
+
+(defun hi-priority-word (prep-phrase)
+  (let* ((max-prior-word (car prep-phrase))
+	 (max-prior (get-prior (car prep-phrase))))
+    (loop for i in prep-phrase do
+	 (if (not (equal (get-prior i) nil))
+	     (if (> (get-prior i) max-prior)
+		 (progn
+		   (setq max-prior (get-prior i))
+		   (setq max-prior-word i)))))
+    max-prior-word))     
+	   
 (defun search-answer (key)
   (let* ((query-to-key (concatenate 'string
 				      "SELECT phrase FROM phrases INNER JOIN keywords ON 
@@ -44,74 +62,48 @@
 (defun answer (phrase)
   (search-answers (prepare-phrase phrase)))
 
-(defun learn (key)
-  (let* ((query-for-key (concatenate 'string
-				     "SELECT id_key FROM keywords WHERE word = \""
-				     (princ-to-string key)
-				     "\";")))
-    (if (equal (caaar (cl-mysql:query query-for-key)) nil) ; Если нет ключа в базе
-	(progn
-	  ;;(print "Добавляю несуществующий ключ в базу")
-	  (let* ((query-insert (concatenate 'string
-					    "INSERT INTO keywords (word) VALUES (\""
-					    (princ-to-string key)
-					    "\");")))
-	    (cl-mysql:query query-insert))
-	  (let* ((query-get-id (concatenate 'string
-					    "SELECT id_key FROM keywords WHERE word = \""
-					    (princ-to-string key)
-					    "\";"))
-		 (new-id (caaar (cl-mysql:query query-get-id))))
-	    (format t "Что ответить на это?~%")
-	    (let* ((my-answer (read-line))
-		   (query-answer (concatenate 'string
-					      "INSERT INTO phrases (id_key, phrase) VALUES ("
-					      (princ-to-string new-id)
-					      ", "
-					      "\"" 
-					      my-answer
-					      "\""
-					      ");")))
-	      (cl-mysql:query query-answer))))
-	(progn                                         ; Если есть ключ в базе
-	  ;;(print "Ключ есть в базе")
-	  (let* ((query-get-id (concatenate 'string
-					    "SELECT id_key FROM keywords WHERE word = \""
-					    (princ-to-string key)
-					    "\";"))
-		 (key-id (caaar (cl-mysql:query query-get-id)))
-		 (query-for-phrases (concatenate 'string 
-						 "SELECT id_phrase FROM phrases WHERE id_key = \""
-						 (princ-to-string (car key-id)) ;; Проблема тут!!!!!
-						 "\";")))
-	    (if (not (equal (caaar (cl-mysql:query query-for-phrases)) nil)) ; Если есть ответы на ключ
-		(progn
-		  ;;(print "Ответ на ключ:")
-		  (print (search-answer key)))
-		(progn
-		  ;;(print "Нет ответов на заданный ключ")
-		  (let* ((query-get-id (concatenate 'string
-					"SELECT id_key FROM keywords WHERE word = \""
-					(princ-to-string key)
-					"\";"))
-			 (new-id (caaar (cl-mysql:query query-get-id))))
-		    (format t "Что ответить на это?~%")
-		    (let* ((my-answer (read-line))
-			   (query-answer (concatenate 'string
-						      "INSERT INTO phrases (id_key, phrase) VALUES ("
-						      (princ-to-string new-id)
-						      ", "
-						      "\"" 
-						      my-answer
-						      "\""
-						      ");")))
-		      (cl-mysql:query query-answer))))))))))
+
+(defun up-priority (key)
+  (let* ((query-for-up (concatenate 'string
+				    "UPDATE keywords SET priority = priority+1 WHERE word = \""
+				    (princ-to-string key)
+				    "\"")))
+    (if (not (equal key "Эйфьятлайокудль"))
+	(cl-mysql:query query-for-up))))
 
 
 
-(defun answer-learn (phrase)
-  (learn (car (last (prepare-phrase phrase)))))
-  
+(defun get-key-id (key)
+  (let ((query-id (concatenate 'string
+			       "SELECT id_key FROM keywords WHERE word = \""
+			       (princ-to-string key)
+			       "\"")))
+    (car (caaar (cl-mysql:query query-id)))))
+
+(defun test-word (key)
+  (let ((query-for-key (concatenate 'string
+				    "SELECT id_key FROM keywords WHERE word = \""
+				    (princ-to-string key)
+				    "\";")))
+    (if (equal (car (caaar (cl-mysql:query query-for-key))) nil)
+	nil
+	t)))
+
+
+  (let ((prep-phrase (prepare-phrase phrase)))
+    (loop for i in prep-phrase do
+	 (if (equal (test-word i) nil)
+	     (add-key i)))))
+
+
+(defun add-key (key)
+  (let* ((query-insert (concatenate 'string
+				    "INSERT INTO keywords (word) VALUES (\""
+				    (princ-to-string key)
+				    "\");")))
+    (cl-mysql:query query-insert)))
+
+;; Depricated
 (defun get-word ()
   (format t "Введите слово: ~%")
   (let* ((word (read))
@@ -141,10 +133,42 @@
 					"\""
 					");")))
 	(cl-mysql:query query-answer)))))
-	
-(defun get-words (i)
-  (if (> i 0)
+
+
+(defun learn (key)
+  (if (equal (test-word key) nil)
       (progn
-	(get-word)
-	(get-words (- i 1)))))
+	;; No key in db
+	(add-key key)
+	(format t "Что отвечать?~%")
+	(let* ((phrase (read-line)) 
+	       (query-to-add (concatenate 'string 
+					  "INSERT INTO phrases (id_key, phrase) VALUES ("
+					  (princ-to-string (get-key-id key))
+					 ", \""
+					  phrase
+					  "\");")))
+	  (cl-mysql:query query-to-add)))
+      (progn 
+	;; Key exists
+	(up-priority key)
+	(if (equal (car (search-answer key)) nil)
+	    (progn 
+	      ;; No answers
+	      (format t "Что отвечать?~%")
+	      (let* ((phrase (read-line)) 
+		     (query-to-add (concatenate 'string 
+						"INSERT INTO phrases (id_key, phrase) VALUES ("
+						(princ-to-string (get-key-id key))
+						", \""
+						phrase
+						"\");")))
+		(cl-mysql:query query-to-add)))
+	    (format t "~A" (car (search-answer key)))))))
+
+
+(defun chat (phrase)
+  (let ((key (hi-priority-word 
+	      (prepare-phrase phrase))))
+    (learn key))
 
